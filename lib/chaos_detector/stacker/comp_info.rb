ee09584039c33ae0ef4550184cace8e9c46a041b
Steven Miers <6759449+git-steven@users.noreply.github.com>
2020-09-07 09:16:55 -0500

    Add CompInfo base class and edge for chaos-specific attributes.

---
+++ lib/chaos_detector/stacker/comp_info.rb
@@ -0,0 +1,29 @@
+require 'chaos_detector/chaos_utils'
+
+module ChaosDetector
+  module Stacker
+    # Base class for Component (Module, FN) Infos
+    class CompInfo
+      attr_accessor :path
+      attr_accessor :name
+      attr_accessor :info
+
+      def initialize(name:, info: nil, path: nil)
+        @name = name
+        @path = path
+        @info = info
+      end
+
+      def ==(other)
+        self.name == other.name &&
+        self.path == other.path &&
+        self.info == other.info
+      end
+
+      def to_s
+        "#{name}: #{path} - #{info}"
+      end
+
+    end
+  end
+end
